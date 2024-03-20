b7eae058a7af8956d9856bcc03d3638b083daffd
EugeneSamokhval <91789484+EugeneSamokhval@users.noreply.github.com>
2024-03-20 20:43:43 +0300

    Changes to be committed: 	modified:   __pycache__/pipeline.cpython-310.pyc 	modified:   pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -7,7 +7,7 @@
 
 import machineoperations as mo
 import consts
-import prett
+from prettytable import PrettyTable
 
 
 def pipeline(vector_1, vector_2):
@@ -27,8 +27,8 @@ def pipeline(vector_1, vector_2):
                     temp_result)-2], vector_1[index], temp_result[len(temp_result)-1], index])
                 tic_counter += 1
             else:
-                pipe_data_collector.append([tic_counter, 'sum', mo.create_array(
-                    consts.NUMSIZE), mo.create_array(consts.NUMSIZE), mo.create_array(consts.NUMSIZE), index])
+                pipe_data_collector.append([tic_counter, 'sum', temp_result[len(
+                    temp_result)-2], mo.create_array(consts.NUMSIZE), temp_result[len(temp_result)-1], index])
             temp_result.append(mo.shear_right(
                 temp_result[len(temp_result)-1], 1))
             pipe_data_collector.append(
@@ -39,55 +39,43 @@ def pipeline(vector_1, vector_2):
             temp_result)-2], 1, temp_result[len(temp_result)-1], index])
         result.append(temp_result)
     pipe_data_collector.sort(key=lambda x: x[0])
-    pipeview(pipe_data_collector, tic_counter)
+    pipeview(pipe_data_collector, len(vector_1))
     return result
 
 
-def pipeview(content: list, tic_counter):
-    # pair = []
-    # late_list = []
-    # index = 0
-    # tic = 0
-    # while index < len(content):
-    #     stage = 1
-    #     if len(pair) == 0:
-    #         pair.append(content[index])
-    #         content.remove(content[index])
-    #     elif content[index][-1] != pair[0][-1] and content[index][1] != pair[0][1]:
-    #         pair.append(content[index])
-    #         content.remove(content[index])
-    #     else:
-    #         late_list.append(content[index])
-    #         index += 1
-    #     if 0 < len(pair) < 2:
-    #         print("\nТакт ", tic)
-    #         for entry in pair:
-    #             if entry[1] == 'sum':
-    #                 print('Этап ', stage)
-    #                 print('Суммирование чисел: ',
-    #                       entry[2], '+', entry[3], '=', entry[4])
-    #                 stage += 1
-    #             if entry[1] == 'shearl' and entry[-1]:
-    #                 print('Этап ', stage)
-    #                 print('Сдвиг влево: ', entry[2],
-    #                       'на', entry[3], '=', entry[4])
-    #             if entry[1] == 'shearr':
-    #                 print('Этап ', stage)
-    #                 print('Сдвиг вправо: ', entry[2],
-    #                       'на', entry[3], '=', entry[4])
-    #         pair = []
-    #         content = content + late_list
-    #         late_list = []
-    #         tic += 1
-    #         index = 0
-    numbers = [[] for entry in range(content[-1][-1]+1)]
+def pipeview(content: list, vectors_len):
+    numbers = [[] for entry in range(vectors_len)]
     table_array = [[] for index in range(len(numbers))]
     for index in range(len(content)):
         numbers[content[index][-1]].append(content[index])
     step = 0
     for index in range(len(numbers)):
-        table_array[index] = [[] for i in range(step)]
+        table_array[index] = [None for i in range(step)]
         for subindex in range(len(numbers[index])):
-            table_array[index].append(numbers[subindex])
+            table_array[index].append(numbers[index][subindex])
         step += 1
-    print(table_array)
+    maximal_len = max([len(entry) for entry in table_array])
+    for index in range(len(table_array)):
+        while len(table_array[index]) < maximal_len:
+            table_array[index].append(None)
+    for subindex in range(maximal_len):
+        print(200*'-', "\nТакт ", subindex, '\n')
+        for index in range(len(table_array)):
+            entry = table_array[index][subindex]
+            if not entry:
+                print('Ожидает очереди')
+            elif entry[1] == 'sum':
+                stage = subindex + entry[-1]
+                print('Этап ', stage)
+                print('Суммирование чисел: ',
+                      entry[2], '+', entry[3], '=', entry[4])
+            elif entry[1] == 'shearl':
+                stage = subindex + entry[-1]
+                print('Этап ', stage)
+                print('Сдвиг влево: ', entry[2],
+                      'на', entry[3], '=', entry[4])
+            elif entry[1] == 'shearr':
+                stage = subindex + entry[-1]
+                print('Этап ', stage)
+                print('Сдвиг вправо: ', entry[2],
+                      'на', entry[3], '=', entry[4])
